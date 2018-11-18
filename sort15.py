28666efaf24798c6f5f2994a1f1eae3670df531a
souravtecken <souravtecken@gmail.com>
2018-11-18 13:00:38 +0530

    Numbers are now in a random order

--- sort15.py
+++ sort15.py
@@ -6,7 +6,7 @@ bgCOLOR = "WHITE"
 timerWidth=30
 gapWidth=5
 tileWidth=70
-gridSize=4
+gridSize=5
 
 class TILE:
     def __init__(self,x=0,y=0,value=0):
@@ -32,6 +32,15 @@ win=GraphWin("Sort 15", tileWidth*gridSize+gapWidth*(gridSize+1),tileWidth*gridS
 win.setBackground("white")
 
 
+def initTileValues(tiles):
+    numbers=list(range(gridSize*gridSize))
+    print(numbers)
+    for i in range(gridSize):
+        for j in range(gridSize):
+            tiles[i][j].val=random.choice(numbers)
+            numbers.remove(tiles[i][j].val)
+    
+
 def drawGrid(tiles):
     for i in range(gridSize):
         for j in range(gridSize):
@@ -41,7 +50,7 @@ def drawGrid(tiles):
 # Create a 2D list of tiles, assigning the appropriate midpoints and initial ordered values.
 tiles=[[TILE(gapWidth*(j+1)+j*tileWidth+tileWidth//2,gapWidth*(i+1)+i*tileWidth+tileWidth/2,i*4+j) for j in range(gridSize)] for i in range(gridSize)]
 
-
+initTileValues(tiles)
 drawGrid(tiles)
 
 
